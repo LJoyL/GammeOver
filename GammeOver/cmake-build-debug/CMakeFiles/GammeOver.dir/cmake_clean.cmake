0ef959ddb8179b2e89b90601285ce4460c608e5e
Léonounoune <jolyleo.jl@gmail.com>
2019-02-07 22:13:42 +0100

    Mise en place du projet CLion

---
+++ GammeOver/cmake-build-debug/CMakeFiles/GammeOver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GammeOver.dir/main.cpp.o"
+  "GammeOver.pdb"
+  "GammeOver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GammeOver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GammeOver/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jolyl on 07/02/2019.
+//
+
+#include <iostream>
+
+int main()
+{
+    char oui = 'n';
+    scanf("%c", &oui);
+    if (oui == 'o')
+    {
+        system("play Piano.wav");
+    }
+
+
+    return(0);
+}
